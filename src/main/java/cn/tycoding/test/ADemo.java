190c0b8e5c774056a4e27fb65c0a63f99585c803
liuklm <liuklm@yonyou.com>
2019-09-27 19:39:29 +0800

    update4

--- src/main/java/cn/tycoding/test/ADemo.java
+++ src/main/java/cn/tycoding/test/ADemo.java
@@ -1,5 +1,7 @@
 package cn.tycoding.test;
 
 public @interface ADemo {
-    String  value() default  "12";
+    String  value2() default  "12";
+    String  value1() default  "123";
+    String  value3() default  "124";
 }
